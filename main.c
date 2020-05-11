3fdd49e6a1e1136e35ffbea40caf04f841deca9b
Konst B <balcon@inbox.ru>
2020-05-11 22:01:40 +0300

    optimize size

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include "stm8s.h"
 #include <stdio.h>
-#define DQ_PORT GPIOD           // DATA port
-#define DQ_PIN GPIO_PIN_2       // DATA pin
-#define VC_PORT GPIOD           // VC transistor base port
-#define VC_PIN GPIO_PIN_3       // VC transistor base pin
+#define DQ_PIN GPIOD,GPIO_PIN_2       // DATA pin
+#define VC_PIN GPIOD,GPIO_PIN_3       // VC transistor base pin
+
+#define D_490_uS 1568 
+#define D_40_uS 128
+#define D_10_uS 32
 
 //  |             /[100]---[+5V]         
 //  |            C       |
@@ -13,45 +15,42 @@
 // [DQ]------------------[DS18B20]
 //  |
 
-#define N 4
-unsigned short devices[N][8];
-void delay_us(unsigned int us){
-  us*=3.2;
+#define N 2 // amount of devices
+short devices[N][8]; //devises serial numbers
+
+void delay_us(int us){
   while(us) us--;
 }
-void delay_ms(unsigned long ms){
-  ms*=555;
-  while(ms) ms--;
-}
-void sendBit(unsigned short bit){
+
+void sendBit(short bit){
   if(bit){
-    GPIO_WriteLow(DQ_PORT,DQ_PIN);
+    GPIO_WriteLow(DQ_PIN);
     unsigned short i=30; //9us delay for '1'
     while(i) i--;
-    GPIO_WriteHigh(DQ_PORT,DQ_PIN);
+    GPIO_WriteHigh(DQ_PIN);
     i=200;
     while(i) i--;
   }else{
-    GPIO_WriteLow(DQ_PORT,DQ_PIN);
+    GPIO_WriteLow(DQ_PIN);
     unsigned short i=250; //64us delay for '0'
     while(i) i--;
-    GPIO_WriteHigh(DQ_PORT,DQ_PIN);
+    GPIO_WriteHigh(DQ_PIN);
   }  
 }
 unsigned short readBit(){
   unsigned short bit=0;
-  GPIO_WriteLow(DQ_PORT,DQ_PIN);
+  GPIO_WriteLow(DQ_PIN);
   unsigned short i=20; // 6us delay
   while(i) i--;
-  GPIO_WriteHigh(DQ_PORT,DQ_PIN);
-  if(GPIO_ReadInputPin(DQ_PORT,DQ_PIN)) bit=1;
+  GPIO_WriteHigh(DQ_PIN);
+  if(GPIO_ReadInputPin(DQ_PIN)) bit=1;
   i=240; // delay to 60+us
   while(i) i--;
   return(bit);
 }
 unsigned short readByte(){
   unsigned short a=0;
-  for(unsigned short i=0;i<8;i++){
+  for(short i=0;i<8;i++){
     a=a>>1;
     if(readBit()) a+=128;
   }
@@ -62,35 +61,35 @@ void sendByte(unsigned short byte){
 }
 short reset(){
   short status=0;
-  GPIO_WriteLow(DQ_PORT,DQ_PIN); // pull down DQ for init/reset
-  delay_us(490);
-  GPIO_WriteHigh(DQ_PORT,DQ_PIN); // release DQ
-  delay_us(40);
-  status=GPIO_ReadInputPin(DQ_PORT,DQ_PIN);// check answer from DS18
-  delay_us(450);
+  GPIO_WriteLow(DQ_PIN); // pull down DQ for init/reset
+  delay_us(D_490_uS);
+  GPIO_WriteHigh(DQ_PIN); // release DQ
+  delay_us(D_40_uS);
+  status=GPIO_ReadInputPin(DQ_PIN);// check answer from DS18
+  delay_us(D_490_uS);
   return(!status);
 }
 
 void setConfigAll(){
   if(reset()){
     sendByte(0xCC); // to all devices 
-    sendByte(0x4E); // write to ROM 3 config bytes
-    sendByte(0x4B); //  alarm trigger register (default)
-    sendByte(0x46); //  alarm trigger register (default)
-    sendByte(0x1F); //  set low resolution (quick convetr T)
+    sendByte(0x4E); // write to ROM config bytes
+    sendByte(0x4B); // byte1: alarm trigger register (default)
+    sendByte(0x46); // byte2: alarm trigger register (default)
+    sendByte(0x1F); // byte3: set low resolution (quick convetr T)
   }
 }
 void searchSerialNums(){
-  unsigned short snBit;
-  unsigned short iBit;
+  short snBit; // serial number bit, sended by device
+  short iBit; // inverted serial number bit, sended by device
   short lastFork=-1;
   short rightFork=-1;
-  for(unsigned short j=0;j<N;j++){
+  for(short j=0;j<N;j++){
     if(reset()){
       sendByte(0xF0); // search SN
       for(short i=0;i<64;i++){
-        unsigned short byteN=i/8;
-        unsigned short bitN=i%8;
+        short byteN=i/8;
+        short bitN=i%8;
         if((bitN)==0) devices[j][byteN]=0;
         snBit=readBit();
         iBit=readBit();
@@ -109,52 +108,57 @@ void searchSerialNums(){
     }
   }
 }
-short getTemp(unsigned short* sn){
-  GPIO_WriteLow(VC_PORT,VC_PIN);
+short getTemp(short* sn){
+  __disable_interrupt();
+  GPIO_WriteLow(VC_PIN);
   short temp=0;
   if(reset()){
-    sendByte(0x55); // to all devices
-    for(unsigned short i=0;i<8;i++) sendByte(sn[i]);
+    sendByte(0x55); // to one devices
+    for(unsigned short i=0;i<8;i++) sendByte(sn[i]); // serial number of device
     sendByte(0xBE); // read memory
-      GPIO_WriteHigh(VC_PORT,VC_PIN);
-      delay_us(10);
-        GPIO_WriteLow(VC_PORT,VC_PIN);
+    GPIO_WriteHigh(VC_PIN);
+    delay_us(D_10_uS);
+    GPIO_WriteLow(VC_PIN);
     unsigned short lsb=readByte(); // only first two bytes
     unsigned short msb=readByte(); 
     reset();
     temp=(msb<<4)+(lsb>>4);
   }
+  __enable_interrupt();
   return(temp);
 }
 void convertTempAll(){
+  __disable_interrupt();
   if(reset()){
     sendByte(0xCC); // to all devices
     sendByte(0x44); // conert T
-    GPIO_WriteHigh(VC_PORT,VC_PIN);
+    GPIO_WriteHigh(VC_PIN);
   }
+  __enable_interrupt();
 }
 void initTempDevices(){
-  GPIO_Init(VC_PORT,VC_PIN,GPIO_MODE_OUT_PP_LOW_SLOW);
-  GPIO_Init(DQ_PORT,DQ_PIN,GPIO_MODE_OUT_OD_HIZ_SLOW);
-  
+  __disable_interrupt();
+  GPIO_Init(VC_PIN,GPIO_MODE_OUT_PP_LOW_SLOW);
+  GPIO_Init(DQ_PIN,GPIO_MODE_OUT_OD_HIZ_SLOW);
   searchSerialNums();
   setConfigAll();
+  __enable_interrupt();
 }
 void main() {
   
   CLK_SYSCLKConfig(CLK_PRESCALER_HSIDIV1);
- /* initTempDevices();
+  initTempDevices();
   short temp;
   while(1){
     convertTempAll();
-    delay_ms(95); // check delay for convert T resolution
-   for(unsigned short i=0;i<N;i++){
+    for(long i=0;i<52725;i++); // delay 95mS until temp converted (select by resolution)
+    for(unsigned short i=0;i<N;i++){
       temp=getTemp(devices[i]);
       putchar(temp/10+48);
       putchar(temp%10+48);
       putchar(' ');
    }
-   putchar('\n');*/
+   putchar('\n');
 }
  
 }
